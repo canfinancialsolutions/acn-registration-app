ac57608709de359be269176015ec37d520546802
canfinancialsolutions <canfinancialsolutions@gmail.com>
2026-01-28 18:33:34 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ export default function App() {
     <div className="page">
       <RegistrationForm />
       <footer className="footer">
-        <span>© {new Date().getFullYear()}CAN Care & Advancement Network</span>
+        <span>©  {new Date().getFullYear()}CAN Care & Advancement Network</span>
       </footer>
     </div>
   );
